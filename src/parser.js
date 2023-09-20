32f79ffd0ee4ff2f4c5ef5d56770bbd2e2932758
seog-jun <shong69@myseneca.ca>
2023-09-19 20:11:51 -0400

    Added mdParseToHtml function to convert .md to .html

--- src/parser.js
+++ src/parser.js
@@ -30,10 +30,55 @@ function parseToHtml(content, filename) {
     </html>`;
 }
 
+function mdParseToHtml(content, filename) {
+  let title = parseFileName(filename);
+
+  // to remove \r\n at the end of content
+  let str = content.slice(-1).toString();
+  str = str.slice(0, -2);
+  content.pop();
+  content.push(str);
+
+  // to convert md lines to html tags
+  const htmlContent = content
+    .map((line) => {
+      if (line.startsWith("# ")) {
+        var slicedLine = line.slice(2);
+        return `<h1>${slicedLine}</h1>`;
+      } else if (line.startsWith("## ")) {
+        var slicedLine = line.slice(3);
+        return `<h2>${slicedLine}</h2>`;
+      } else if (line.startsWith("### ")) {
+        var slicedLine = line.slice(3);
+        return `<h3>${slicedLine}</h3>`;
+      } else if (line.startsWith("_") && line.endsWith("_")) {
+        var slicedLine = line.slice(1);
+        slicedLine = slicedLine.slice(0, -1);
+        return `<i>${slicedLine}</i>`;
+      } else {
+        return `<p>${line}</p>`;
+      }
+    })
+    .join("\n");
+
+  return `
+    <!doctype html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    ${htmlContent}
+    </body>
+    </html>`;
+}
+
 function parseFileName(path) {
   const filenameRegex = /[\\/](?:([^\\/]+))\.\w+$/;
   let name = path.match(filenameRegex);
   return name[1];
 }
 
-export { parseToHtml, parseFileName };
+export { parseToHtml, parseFileName, mdParseToHtml };
